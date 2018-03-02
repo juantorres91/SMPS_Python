6f409e652d80a1df394ae6d288a0a474bbf79c7a
juantorres91 <jj.torres1443@uniandes.edu.co>
2018-03-02 12:54:15 -0500

    modified:   __init__.py 	modified:   __init__.pyc

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from SMPS.DeModel import DataModel
-from SMPS.Stoch   import StoModel
+from smps_python.DeModel import DataModel
+from smps_python.Stoch   import StoModel
 from ModelGen     import (DeModel,ModelInstance)
 from Ph_Init      import ph_init
