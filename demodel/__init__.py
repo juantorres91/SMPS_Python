bac48151923a168dc1b843f58e0ab5d1c6074ded
Juan Torres <jj.torres1443@uniandes.edu.co>
2018-03-02 12:44:48 -0500

    deleted:    demodel/__init__.py 	deleted:    demodel/bounds.py 	deleted:    demodel/bounds.py~ 	deleted:    demodel/columns.py 	deleted:    demodel/corsections.py 	deleted:    demodel/rhs.py 	deleted:    demodel/rows.py 	deleted:    stoch/__init__.py 	deleted:    stoch/formats/__init__.py 	deleted:    stoch/formats/block.py 	deleted:    stoch/formats/block.py~ 	deleted:    stoch/utils/__init__.py 	deleted:    stoch/utils/dcase.py 	deleted:    stoch/utils/dcase.py~ 	deleted:    stoch/utils/stocase.py 	deleted:    stoch/utils/stocase.py~ 	deleted:    stoch/utils/treeutils.py 	deleted:    stoch/utils/treeutils.py~ 	deleted:    time_file/__init__.py 	deleted:    time_file/__init__.py~ 	deleted:    time_file/tim_utils.py 	deleted:    time_file/tim_utils.py~ 	deleted:    time_file/watc-10.tim

--- demodel/__init__.py
+++
@@ -1,9 +0,0 @@
-#----------------------------------
-# Determinist model section analyser 
-#----------------------------------
-
-from ROWS     import Rows
-from COLUMNS  import Columns
-from RHS      import Rhs
-from BOUNDS   import Bounds
-from Sections import Cor_Section
--- time_file/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ['time_sorting', 'time_periods']
-__version__ = '0.1'
-__author__ = 'Juan J. Torres'
-
-from time_utils import time_sorting, time_periods 
